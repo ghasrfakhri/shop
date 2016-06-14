a052a1a29b98152ed81f9a7d4dbe61e5f31a3a29
Iman <ghasrfakhri@gmail.com>
2016-06-14 07:59:42 -0700

    Init Commit

--- includes/init.php
+++ includes/init.php
@@ -4,8 +4,8 @@ session_start();
 require __DIR__ . '/../vendor/autoload.php';
 
 spl_autoload_register(function($cn) {
-    $file = __DIR__ . "/$cn.class.php";
-    if (file_exists($file)) {
-        require $file;
+    $filename = __DIR__ . "/$cn.class.php";
+    if (file_exists($filename)) {
+        require $filename;
     }
 });
