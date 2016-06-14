f982224f543d639cee3015ef65c17e44de5fb52c
Administrator <Administrator@10.10.105.10>
2016-06-14 07:57:04 -0700

    Init Commit

---
+++ includes/Db.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Db extends mysqli {
+
+    static private $db;
+
+    private function __construct() {
+        parent::__construct("localhost", "root", "", "shop_db");
+    }
+
+    static function get() {
+        if (self::$db === null) {
+            self::$db = new Db();
+        }
+
+        return self::$db;
+    }
+
+}
