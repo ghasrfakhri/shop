33f082fea8b22caa82d2b832b0272d36f868990f
Administrator <Administrator@10.10.105.10>
2016-06-21 07:22:41 -0700

    Simple error handling

--- includes/MyMysqli.class.php
+++ includes/MyMysqli.class.php
@@ -8,7 +8,24 @@ class MyMysqli extends mysqli {
      * @return mysqli_result
      */
     public function query($query) {
-        return parent::query($query);
+        $result = parent::query($query);
+        if ($result === false) {
+            if (DEBUG) {
+                echo "Db Error:" . $this->error . "<br>";
+                echo "Db Error No:" . $this->errno . "<br>";
+                echo "Query:" . $query . "<br>";
+                $debugTrace = debug_backtrace();
+                echo "File:" . $debugTrace[0]['file'] . "<br>";
+                echo "Line:" . $debugTrace[0]['line'] . "<br>";
+
+                var_dump($debugTrace);
+                exit();
+            } else {
+                echo "An error occured";
+                exit();
+            }
+        }
+        return $result;
     }
 
 }
